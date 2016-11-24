ef47bbb7d08bffa3010dcab5bc5c00a8d4d2e600
ViktorZykov <zich2015@ukr.net>
2016-11-24 23:59:05 +0200

    JavaRush

---
+++ src/com/javarush/test/level05/lesson07/task02/Cat.java
@@ -0,0 +1,51 @@
+package com.javarush.test.level05.lesson07.task02;
+
+/* Создать класс Cat
+Создать класс Cat (кот) с пятью инициализаторами:
+- Имя,
+- Имя, вес, возраст
+- Имя, возраст (вес стандартный)
+- вес, цвет, (имя, адрес и возраст неизвестны, это бездомный кот)
+- вес, цвет, адрес ( чужой домашний кот)
+Задача инициализатора – сделать объект валидным. Например, если вес неизвестен, то нужно указать какой-нибудь средний вес.
+ Кот не может ничего не весить. То же касательно возраста. А вот имени может и не быть (null). То же касается адреса: null.
+*/
+
+public class Cat
+{
+    private String name;
+    private String adress;
+    private int age;
+    private int weight;
+    private String color;
+
+    public void initialize(String name)
+    {
+        this.name = name;
+    }
+    public void initialize(String name, int weight, int age)
+    {
+        this.name = name;
+        this.weight = weight;
+        this.age = age;
+    }
+    public void initialize(String name, int age)
+    {
+        this.name = name;
+        this.weight = 2;
+        this.age = age;
+    }
+    public void initialize(int weight, String color)
+    {
+        this.weight = weight;
+        this.color = color;
+        this.age = 2;
+    }
+    public void initialize(int weight, String color, String adress)
+    {
+        this.weight = weight;
+        this.color = color;
+        this.adress = adress;
+        this.age = 2;
+    }
+}
