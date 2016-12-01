d6a941d5444c53d35cde0694c93cb949af6723ae
ViktorZykov <zich2015@ukr.net>
2016-12-01 23:47:34 +0200

    java

--- src/com/javarush/test/level06/lesson08/task03/Util.java
+++ src/com/javarush/test/level06/lesson08/task03/Util.java
@@ -9,7 +9,7 @@ public class Util
 {
     public static double getDistance(int x1, int y1, int x2, int y2)
     {
-        double a = ((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        double a = (( x2 - x1 ) * ( x2 - x1 ) + ( y2 - y1 )*( y2 - y1 ));
        return Math.sqrt( a );//напишите тут ваш код
 
     }
