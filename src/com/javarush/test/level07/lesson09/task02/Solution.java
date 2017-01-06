86c62e2798d163dc8777668b94ec16afea252ee6
Victor Zykov <zich2015@ukr.net>
2017-01-07 00:17:43 +0200

    java

---
+++ src/com/javarush/test/level07/lesson09/task02/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.test.level07.lesson09.task02;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/* Слова в обратном порядке
+Введи с клавиатуры 5 слов в список строк. Удали 3 - ий элемент списка, и выведи оставшиеся элементы в обратном порядке.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//ввод строк с клавиатуры
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 5; i++)
+        {
+            String s = reader.readLine();
+            list.add(s);
+        }
+        list.remove(2);
+        for (int i = 0; i < list.size(); i++)// for (int i = LINES_QTY - 1; i >= 0; i-- )
+        {
+            int j = list.size() - i - 1;//так типа тоже можно
+            System.out.println( list.get(j) );// System.out.println(list.get(i));
+        }
+    }
+}
